bd0631a73191da4be9cb5e9983f5aee7ade703f9
ainkay <ain.kay@kalvium.community>
2025-06-11 14:12:43 +0530

    edit

--- AttendanceSystem/src/com/school/main.java
+++ AttendanceSystem/src/com/school/main.java
@@ -1,6 +1,6 @@
 package com.school;
-public class main{
+public class Main{
     public static void main(String[]args){
-        System.out.println("Welcome to the Attendance System!");
+        System.out.println("Welcome to the Attendance System!!");
     }
 }
